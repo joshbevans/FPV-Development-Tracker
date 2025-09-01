375cdbac7950e5d58c27dffbad2432e268ca7d43
Joshua Bevans <jiabevans@gmail.com>
2025-09-01 20:09:30 +0100

    Add dbContext and connectionString

---
+++ FPVDevelopment/Components/Data/FPVDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FPVDevelopment.Components.Data
+{
+    public class FPVDbContext : DbContext
+    {
+        public FPVDbContext(DbContextOptions<FPVDbContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
